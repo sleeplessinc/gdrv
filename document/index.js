80ba2af73f9818372d008d5c6771a33c389346ba
Joe Hitchens <joe@sleepless.com>
2017-12-06 22:00:36 -0800

    -

---
+++ document/index.js
@@ -0,0 +1,8 @@
+
+var fetch = require('google-docs-fetch');
+
+exports.get = function get(key, cb) => {
+	fetch(key, cb);
+};
+
+
---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+var sub_modules = {};
+
+module.exports = function(sub_module_name) {
+	var sub_module = sub_modules[sub_module_name];
+	if(!sub_module) {
+		try {
+			sub_module = require("./sub_modules/"+sub_module_name);
+			sub_modules[sub_module_name] = sub_module;
+		} catch(e) { }
+	}
+	return sub_module;
+};
+
+
