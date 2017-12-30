905792bb4c95b97f236bf4c600e68c414a86c4a2
Joe Hitchens <joe@sleepless.com>
2017-12-29 21:01:40 -0800

    -

--- sub_modules/document/index.js
+++ sub_modules/document/index.js
@@ -1,27 +1,9 @@
 
 var fetch = require('google-docs-fetch');
 
-var Cache = require('cache');
-
-
 function get(key, cb) {
 	fetch(key, cb);
 };
 
-
-var cache = {};
-
-function get_cache = function(key, secs, cb) {
-
-	var doc = cache[key];
-	if(doc) {
-		// found in cache, return it.
-		cb(null, doc);
-		return;	
-	}
-
-}
-
 exports.get = get;
-exports.get_cache = get_cache;
 
